f98dec4eb387af795090641938897454e28f5b4d
tatcn8 <tatcn8@gmail.com>
2022-04-06 16:18:33 +0000

    cleaned up the admin views

--- app/admin/months_admin.rb
+++ app/admin/months_admin.rb
@@ -5,23 +5,34 @@ Trestle.resource(:months) do
 
   # Customize the table columns shown on the index view.
   #
-  # table do
-  #   column :name
-  #   column :created_at, align: :center
-  #   actions
-  # end
+  table do
+    column :month
+    column :year
+    column :user
+    column :created_at
+    column :id
+    actions
+  end
 
   # Customize the form fields shown on the new/edit views.
   #
-  # form do |month|
-  #   text_field :name
-  #
-  #   row do
-  #     col { datetime_field :updated_at }
-  #     col { datetime_field :created_at }
-  #   end
-  # end
+  form do |month|
+    tab :month do
+      text_field :month
+      text_field :year
+      select :status, ["public"] 
+      collection_select :user_id, User.all, :id, :email
+    end
 
+    tab :incomes, badge: month.incomes.size do
+      table IncomesAdmin.tables[:index], collection: month.incomes 
+      concat admin_link_to("New Income", admin: :incomes, action: :new, params: { month_id: month }, class: "btn btn-success")
+    end
+    tab :expenses, badge: month.expenses.size do
+      table ExpensesAdmin.tables[:index], collection: month.expenses
+      concat admin_link_to("New Expense", admin: :expenses, action: :new, params: { month_id: month }, class: "btn btn-success")
+    end
+  end
   # By default, all parameters passed to the update and create actions will be
   # permitted. If you do not have full trust in your users, you should explicitly
   # define the list of permitted parameters.
