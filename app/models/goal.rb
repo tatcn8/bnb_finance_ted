87221021188586e00a385dd59779ac3c30df3b3c
tatcn8 <tatcn8@gmail.com>
2022-04-20 15:58:51 +0000

    fixed a few hrefs that were formatted poorly and added goals admin page

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,15 @@
 class Goal < ApplicationRecord
-    belongs_to :user
-    validates :title, presence: true
+  belongs_to :user
+  validates :title, presence: true
+
+
+  def amount_left_to_save
+    goal_value - initial_value
+  end
+
+  def amount_per_month
+    (goal_value - initial_value)/(years_to_save * 12)
+  end
+
+
 end
