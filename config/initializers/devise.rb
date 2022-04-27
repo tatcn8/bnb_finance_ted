18cb792710af8912e495517ef8f089421db33647
tatcn8 <tatcn8@gmail.com>
2022-04-27 15:40:33 +0000

    updated database to include booleans for charts in the user model, also updated my user registration form. Now I plan to generate a new devise controller, so I want to save my work

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,35 @@
-# frozen_string_literal: true
+class TurboFailureApp < Devise::FailureApp
+  def respond
+    if request_format == :turbo_stream
+      redirect
+    else
+      super
+    end
+  end
+
+  def skip_format?
+    %w(html turbo_stream */*).include? request_format.to_s
+  end
+end
+
+class TurboController < ApplicationController
+  class Responder < ActionController::Responder
+    def to_turbo_stream
+      controller.render(options.merge(formats: :html))
+    rescue ActionView::MissingTemplate => error
+      if get?
+        raise error
+      elsif has_errors? && default_action
+        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+      else
+        redirect_to navigation_location
+      end
+    end
+  end
+
+  self.responder = Responder
+  respond_to :html, :turbo_stream
+end
 
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
@@ -23,12 +54,6 @@ class TurboFailureApp < Devise::FailureApp
 end
 
 Devise.setup do |config|
-  # The secret key used by Devise. Devise uses this key to generate
-  # random tokens. Changing this key will render invalid all existing
-  # confirmation, reset password and unlock tokens in the database.
-  # Devise will use the `secret_key_base` as its `secret_key`
-  # by default. You can change it below and use your own secret key.
-  # config.secret_key = '802d9fdce494917589fadbad48dc0d1f5766604d44ef1f65cbbcc8bce6c84ab41c20fa480d756869fa51c5a2a0754059b4e7543df98d0d013a5b48a8961db338'
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -319,7 +344,8 @@ Devise.setup do |config|
 
   # ==> Configuration for :registerable
   config.navigational_formats = ['*/*', :html, :turbo_stream]
-  # When set to false, does not sign a user in automatically after their password is
-  # changed. Defaults to true, so a user is signed in automatically after changing a password.
-  # config.sign_in_after_change_password = true
+  # ==> Warden configuration
+  config.warden do |manager|
+    manager.failure_app = TurboFailureApp
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_19_173016) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_27_144632) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_19_173016) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "calculates", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "expected_expenses", force: :cascade do |t|
@@ -129,6 +134,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_19_173016) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false, null: false
+    t.boolean "display_overall", default: true, null: false
+    t.boolean "display_last_month", default: true, null: false
+    t.boolean "display_income_summary", default: true, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
